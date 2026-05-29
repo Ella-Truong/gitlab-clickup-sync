5dd1304e57beb6001e197215ef8a9f976b499ac0
Ella Truong <truonghongnha208@gmail.com>
2026-05-28 17:18:48 -0700

    feat: integrate RabbitMQ publisher

--- webhook-server/src/controllers/gitlab.controller.ts
+++ webhook-server/src/controllers/gitlab.controller.ts
@@ -1,13 +1,22 @@
 import { Request, Response } from "express";
+import { publishMessage } from "../services/rabbitmq.service";
 
 export const handleGitLabWebhook = async (
     req: Request,
     res: Response,
 ) => {
-    console.log(req.body);
+    try {
+        await publishMessage(req.body);
 
-    return res.status(200).json({
-        success: true,
-        message: "GitLab webhook received"
-    })
-}
+        return res.status(200).json({
+            success: true,
+            message: "Webhook published"
+        });
+    }catch(error){
+        console.error(error);
+
+        return res.status(500).json({
+            success: false
+        });
+    }
+};
