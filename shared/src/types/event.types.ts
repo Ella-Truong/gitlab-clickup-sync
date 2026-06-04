f3ba9fee5854e94d44714c93922c5566747b2431
Ella Truong <truonghongnha208@gmail.com>
2026-06-04 12:06:35 -0700

    refactor: extract shared event contract

---
+++ shared/src/types/event.types.ts
@@ -0,0 +1,17 @@
+/**
+ * Define application events published to RabbitMQ
+ */
+
+export type GitLabEventType = "issue-assigned" | "push" | "merge-request";
+
+export interface GitLabEvent {
+    source: "gitlab";
+    eventType: GitLabEventType;
+    projectId?: number;
+    projectName?: string;
+    author?: string;
+    title?: string;
+    issueId?: number;
+    description?: string;
+    commitCount?: number;
+}
---
+++ shared/src/types/gitlab.types.js
@@ -0,0 +1,6 @@
+"use strict";
+/**
+ * Defines TypeScript types for GitLab webhook payloads.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+//# sourceMappingURL=gitlab.types.js.map
