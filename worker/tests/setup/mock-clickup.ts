0bf68b776d44f834848563a77a1392db7b238da2
Ella Truong <truonghongnha208@gmail.com>
2026-06-06 11:43:28 -0700

    test: setup jest for e2e testing

---
+++ worker/tests/setup/mock-clickup.ts
@@ -0,0 +1,8 @@
+/**
+ * Replaces the real ClickUp service during tests
+ * Instead: worker --> real ClickUp service --> real ClickUp APIs
+ * Mock service: worker --> mock ClickUp service --> fake functions
+ * TIPS: only mock EXPORTED functions
+ */
+
+
