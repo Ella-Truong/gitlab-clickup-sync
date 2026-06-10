ba8de1373cd86cfa50782d76044144860a108f95
Ella Truong <truonghongnha208@gmail.com>
2026-06-10 08:59:47 -0700

    feat: add health check for worker server

--- worker/src/worker.ts
+++ worker/src/worker.ts
@@ -1,8 +1,20 @@
 //start the worker
 import dotenv from "dotenv";
+import express from "express";
 import { startConsumer } from "./consumers/gitlabEventConsumer";
 
 dotenv.config();
+const app = express();
+
+app.get("/", (_,res) => {
+    res.send("Worker is running")
+});
+
+const PORT = process.env.PORT || 10000;
+
+app.listen(PORT, () => {
+    console.log(`Health server running on port ${PORT}`)
+});
 
 async function bootstrap(){
     await startConsumer();
