65cc90f6b8d5d34260c53dfb4dbac71a64566bc8
Ella Truong <truonghongnha208@gmail.com>
2026-06-12 22:11:09 -0700

    chore: recheck dependencies

--- shared/src/types/event.types.ts
+++ shared/src/types/event.types.ts
@@ -5,7 +5,7 @@
 import { GitHubPayload } from "./github.types";
 
 export enum GitHubEventType {
-    ISSUE_ASSIGNED = "issue.assgined",
+    ISSUE_ASSIGNED = "issue.assigned",
     PUSH_RECEIVED = "push.received",
     PULL_REQUEST_OPENED = "pull_request.opened",
     PULL_REQUEST_MERGED = "pull_request.merged",
