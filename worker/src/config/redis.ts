c65115b7390557a2bc411fb573ab5fecc9d28520
Ella Truong <truonghongnha208@gmail.com>
2026-06-13 22:34:05 -0700

    feat: update redis connection

--- worker/src/config/redis.ts
+++ worker/src/config/redis.ts
@@ -1,5 +1,19 @@
 import Redis from "ioredis";
 
 export const redis = new Redis(
-    process.env.REDIS_URL!,
+    process.env.REDIS_URL!, {
+        maxRetriesPerRequest: null
+    }
 )
+
+redis.on("connect", () => {
+    console.log("Connect to Redis")
+})
+
+redis.on("ready",() => {
+    console.log("Redis ready")
+})
+
+redis.on("error", (err) => {
+    console.error("Redis error: ", err)
+})
