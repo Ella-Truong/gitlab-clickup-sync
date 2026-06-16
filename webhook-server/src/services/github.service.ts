1838c1d992231a321d3b4078df8f3b0edae57825
Ella Truong <ellatruong95@gmail.com>
2026-06-16 11:01:46 -0700

    feat: update event types

--- webhook-server/src/services/github.service.ts
+++ webhook-server/src/services/github.service.ts
@@ -1,99 +1,130 @@
-/**
- * validate GitLab payload
- * transform data
- * publish event to RabbitMQ
- */
-
 import { publishMessage } from "./rabbitmq.service";
-import { GitHubPayload } from "../../../shared/src/types/github.types";
-import { GitHubEventType, GitHubEvent } from "../../../shared/src/types/event.types";
 
-//validate payload
+import {
+    GitHubIssuePayload,
+    GitHubPullRequestPayload,
+    GitHubPushPayload,
+    GitHubPayload,
+} from "../../../shared/src/types/github.types";
+
+import {
+    GitHubEvent,
+    GitHubEventType,
+} from "../../../shared/src/types/event.types";
+
+/**
+ * Validate webhook payload
+ */
 const validatePayload = (
     webhookType: string,
     payload: GitHubPayload,
 ): void => {
     switch (webhookType) {
         case "issues":
-            if(!("issue" in payload)){
-                throw new Error("Missing issue payload")
+            if (!("issue" in payload)) {
+                throw new Error("Missing issue payload");
             }
 
-            if(!payload.issue.id){
-                throw new Error("Missing issue.id")
-            } 
+            if (!payload.issue.id) {
+                throw new Error("Missing issue.id");
+            }
 
-            if(!payload.issue.title){
-                throw new Error("Missing issue.title")
+            if (!payload.issue.title) {
+                throw new Error("Missing issue.title");
             }
 
             break;
 
         case "push":
-            if(!("commits" in payload)){
-                throw new Error("Missing commits payload")
+            if (!("commits" in payload)) {
+                throw new Error("Missing commits payload");
             }
 
-            if(!payload.commits.length){
-                throw new Error("Push event contains no commits")
+            if (!payload.commits.length) {
+                throw new Error("Push event contains no commits");
             }
 
             break;
 
         case "pull_request":
-            if(!("pull_request" in payload)){
-                throw new Error("Missing pull_request payload")
+            if (!("pull_request" in payload)) {
+                throw new Error("Missing pull_request payload");
             }
 
-            if(!payload.pull_request.id){
-                throw new Error("Missing pull_request.id")
+            if (!payload.pull_request.id) {
+                throw new Error("Missing pull_request.id");
             }
 
-            if(!payload.pull_request.title){
-                throw new Error("Missing pull_request.title")
+            if (!payload.pull_request.title) {
+                throw new Error("Missing pull_request.title");
             }
 
             break;
-        
-        default: throw new Error(`Unsupported event type: ${webhookType}`)
-    }
-}
 
-//convert GitHub webhook into business event
-const determineEventType = (
+        default:
+            throw new Error(`Unsupported event type: ${webhookType}`);
+    }
+};
+
+/**
+ * Build a strongly typed business event
+ */
+const buildEvent = (
     webhookType: string,
-    payload: GitHubPayload
-): GitHubEventType => {
-    if(webhookType === "issues" && "action" in payload && payload.action === "assigned"){
-        return GitHubEventType.ISSUE_ASSIGNED;
+    payload: GitHubPayload,
+): GitHubEvent => {
+    if (
+        webhookType === "issues" &&
+        "action" in payload &&
+        payload.action === "assigned"
+    ) {
+        return {
+            type: GitHubEventType.ISSUE_ASSIGNED,
+            payload: payload as GitHubIssuePayload,
+        };
     }
 
-    if(webhookType === "push"){
-        return GitHubEventType.PUSH_RECEIVED;
+    if (webhookType === "push") {
+        return {
+            type: GitHubEventType.PUSH_RECEIVED,
+            payload: payload as GitHubPushPayload,
+        };
     }
 
-    if(webhookType === "pull_request" && "action" in payload && payload.action === "opened"){
-        return GitHubEventType.PULL_REQUEST_OPENED;
-    }  
-
-    if(webhookType === "pull_request" && "action" in payload && payload.action === "merged"){
-        return GitHubEventType.PULL_REQUEST_MERGED;
+    if (
+        webhookType === "pull_request" &&
+        "action" in payload &&
+        payload.action === "opened"
+    ) {
+        return {
+            type: GitHubEventType.PULL_REQUEST_OPENED,
+            payload: payload as GitHubPullRequestPayload,
+        };
     }
 
-    throw new Error(`Unsupported event: ${webhookType}`)
-}
+    if (
+        webhookType === "pull_request" &&
+        "pull_request" in payload &&
+        "action" in payload &&
+        payload.action === "closed" &&
+        payload.pull_request.merged === true
+    ) {
+        return {
+            type: GitHubEventType.PULL_REQUEST_MERGED,
+            payload: payload as GitHubPullRequestPayload,
+        };
+    }
 
+    throw new Error(`Unsupported event: ${webhookType}`);
+};
 
 export const processWebhook = async (
     payload: GitHubPayload,
     webhookType: string,
 ): Promise<void> => {
     validatePayload(webhookType, payload);
-    const eventType = determineEventType(webhookType, payload)
-    const event: GitHubEvent = {
-        type: eventType,
-        payload
-    }
+
+    const event = buildEvent(webhookType, payload);
 
     await publishMessage(event);
 };
