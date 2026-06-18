41b82196ec638f27e95e8cecadebc9f971b94d46
Ella Truong <ellatruong95@gmail.com>
2026-06-17 22:20:25 -0700

    feat: test with another status

--- worker/src/services/clickup.service.ts
+++ worker/src/services/clickup.service.ts
@@ -38,7 +38,7 @@ export async function createClickUpTask(
             body: JSON.stringify({
                 name: input.title,
                 markdown_content: input.description,
-                status: "Review",
+                status: "Complete",
             }), 
         }
     );
